2d513f0c8d0f45091b1baf6995646595df9fcb5d
Gabriel Ho <gabalex@uw.edu>
2015-11-09 19:39:38 -0800

    commit 1

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# spotify-template
-This template is an optional starting point for a challenge for the following [course](http://faculty.washington.edu/mikefree/info343/) at the University of Washington.  The instructions are described [here](http://faculty.washington.edu/mikefree/info343/#/challenges/spotify).
+1.	What is the purpose of your project (i.e., why would someone use this app)?
+	When I was spending a lot of time exploring music, there wasn't spotify(even iTunes didn't really have a store).
+	I would use this one website called it-leaked to that would let me search artists and songs and then give
+	me suggestions to related artists and other songs. This was how I basically discovered LIFE. With my project,
+	someone needing new music can use my site to put in a song they really like and find related songs/artists.
+
+2.	What is the URL of this project on your student web-server?
+http://students.washington.edu/gabalex/info343/spotify-template/index.html
+
+3.	Did you receive help from any other sources (classmates, etc.)? If so, please list who.
+	Someone in the TE lab helped me with iterating through my array. I didn't know how to call the object because it was 2 arrays but he
+	said if I just use [0,1,2] it would work. 
+
+4.	Approximately how many hours did it take you to complete this challenge?
+	9 hours. A lot of it was exploring the different API endpoints. An hour and a half was dedicated to finding a bug
+	that turned out to be a misplaced quotation mark..yes..a misplaced quotation..
+
+5.	Did you encounter any problems in this challenge we should warn students about in the future? How can we make the challenge better?
+	I think this challenge was really fun and seeing how my website functions made me really wish I had this back when I was
+	looking for music. I don't think there really is much to warn about, except to have fun with it.
--- app.js
+++ app.js
@@ -1,15 +1,41 @@
 var data;
-var baseUrl = 'https://api.spotify.com/v1/search?type=track&query='
-var myApp = angular.module('myApp', [])
+var baseUrl = 'https://api.spotify.com/v1/search?type=track&query=';
+var trackUrl = 'https://api.spotify.com/v1/tracks/';
+var artistUrl = 'https://api.spotify.com/v1/artists/';
+var myApp = angular.module('myApp', []);
 
 var myCtrl = myApp.controller('myCtrl', function($scope, $http) {
   $scope.audioObject = {}
+
   $scope.getSongs = function() {
     $http.get(baseUrl + $scope.track).success(function(response){
       data = $scope.tracks = response.tracks.items
-      
     })
   }
+
+  $scope.getSuggested = function(track) {
+    $scope.chosenTrack = track;
+    var suggestedTracks = [];
+    var suggestedArtists = [];
+
+    $http.get(trackUrl + track.id).success(function(result) {
+      var artist = result.artists[0];
+      $scope.chosenArtist = artist;
+      $http.get(artistUrl + artist.id + '/related-artists').success(function(relateresult) {
+        for (i = 0; i < 3; i++) {
+          var related = relateresult.artists[i];
+          suggestedArtists.push(related);
+          $http.get(artistUrl + related.id + '/top-tracks?country=SE').success(function(trackresult) {
+            var top = trackresult.tracks[0];
+            suggestedTracks.push(top);
+          })
+        }
+        $scope.suggestedTracks = suggestedTracks;
+        $scope.suggestedArtists = suggestedArtists;
+      })
+    })
+  }
+
   $scope.play = function(song) {
     if($scope.currentSong == song) {
       $scope.audioObject.pause()
@@ -23,6 +49,14 @@ var myCtrl = myApp.controller('myCtrl', function($scope, $http) {
       $scope.currentSong = song
     }
   }
+  $scope.show = function() {
+    $scope.showartist = true;
+  }
+
+  $scope.refresh = function() {
+    $scope.hide = false;
+  }
+
 })
 
 // Add tool tips to anything with a title property
