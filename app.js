e5183f3c03bffc86052f09d74eed67de43540bd2
Gabriel Ho <gabalex@uw.edu>
2015-11-09 23:20:31 -0800

    commit 4

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// This is the javascript file associated with spotify website
 var data;
 var baseUrl = 'https://api.spotify.com/v1/search?type=track&query=';
 var trackUrl = 'https://api.spotify.com/v1/tracks/';
@@ -7,24 +8,29 @@ var myApp = angular.module('myApp', []);
 var myCtrl = myApp.controller('myCtrl', function($scope, $http) {
   $scope.audioObject = {}
 
+  //get the songs associated with the user input
   $scope.getSongs = function() {
     $http.get(baseUrl + $scope.track).success(function(response){
       data = $scope.tracks = response.tracks.items
     })
   }
 
+  //given the selected song, gets the related songs that match
   $scope.getSuggested = function(track) {
     $scope.chosenTrack = track;
     var suggestedTracks = [];
     var suggestedArtists = [];
 
+    //gets the track selected
     $http.get(trackUrl + track.id).success(function(result) {
       var artist = result.artists[0];
       $scope.chosenArtist = artist;
+      //gets the other artists related to the selected song
       $http.get(artistUrl + artist.id + '/related-artists').success(function(relateresult) {
         for (i = 0; i < 3; i++) {
           var related = relateresult.artists[i];
           suggestedArtists.push(related);
+          //gets the top track of the respective artists
           $http.get(artistUrl + related.id + '/top-tracks?country=SE').success(function(trackresult) {
             var top = trackresult.tracks[0];
             suggestedTracks.push(top);
@@ -36,6 +42,7 @@ var myCtrl = myApp.controller('myCtrl', function($scope, $http) {
     })
   }
 
+  //Controls the playing of the song
   $scope.play = function(song) {
     if($scope.currentSong == song) {
       $scope.audioObject.pause()
@@ -49,10 +56,12 @@ var myCtrl = myApp.controller('myCtrl', function($scope, $http) {
       $scope.currentSong = song
     }
   }
+  //shows the div
   $scope.show = function() {
     $scope.showartist = true;
   }
 
+  //refreshes the div to reload new songs
   $scope.refresh = function() {
     $scope.hide = false;
   }
